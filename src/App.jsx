e5e1dea869560731adec59c15647aa4d40ec70e2
sunilcostacloud <sunil@costacloud.com>
2023-09-14 14:12:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageNotFound from './components/PageNotFound';
+
+const App = () => {
+  return (
+    <div>
+      <PageNotFound />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import pageNotFoundImg from "./pageNotFoundImg.jpg"
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <img src={pageNotFoundImg} alt="" style={{ height: "100vh", width: "100%" }} />
+        </div>
+    )
+}
+
+export default PageNotFound
